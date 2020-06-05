234dacc1131c09431461edefc2e76f6f2ccb00cb
berellevy <berellevy@gmail.com>
2020-06-05 13:06:55 -0400

    completed one and 2

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,4 +1,5 @@
-## QUESTION 1
+require 'pry'
+require 'json'
 
 pokemon = [
   {
@@ -60,10 +61,21 @@ pokemon = [
 }
 ]
 
+# pokemon = JSON.parse(pokemon)
 
 # How would you get the url for Bulbasaur's ability?
+pokemon[0][:abilities][0][:ability][:url]
+binding.pry
+
 # How would you return the first pokemon with base experience over 40?
+pokemon.find { |p| p[:base_experience] >= 40}
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
+pokemon.select { |p| p[:base_experience] >= 40}
 # How would you return an array of all of the pokemon's names?
+pokemon.map { |p| p[:name] }
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
+pokemon.any? { |p| p[:weight] >= 60}
 #  whatever method you use should return true if there are any such pokemon, false if not.
+
+
+
--- review-question-2.rb
+++ review-question-2.rb
@@ -1,31 +1,51 @@
 # Finish the implementation of the Car class so it has the functionality described below
-
+require "pry"
 class Car
+  attr_reader :make, :model
+  @@all = []
+  def initialize(hash)
+    @make = hash[:make]
+    @model = hash[:model]
+    @@all << self
+  end
 
+  def self.drive
+    "VROOOOOOOOOOOOM!"
+  end
 
+  def self.all
+    @@all
+    
+  end
+  
 
 end
 
-volvo_lightning = Car.new("Volvo", "Lightning")
-yugo = Car.new("Zastava", "Yugo")
-lada = Car.new("AvtoVAZ", "Lada")
+# volvo_lightning = Car.new("Volvo", "Lightning")
+# yugo = Car.new("Zastava", "Yugo")
+# lada = Car.new("AvtoVAZ", "Lada")
 
-volvo_lightning.make
-#=> "Volvo"
-volvo_lightning.model
-#=> "Lightning"
+# volvo_lightning.make
+# #=> "Volvo"
+# volvo_lightning.model
+# #=> "Lightning"
 
-Car.drive
-# => "VROOOOOOOOOOOOM!"
+# Car.drive
+# # => "VROOOOOOOOOOOOM!"
 
-Car.all
-#=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
+# Car.all
+# #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+# binding.pry
+
+#BONUS:
 
 volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
 volvo_lightning.make
 #=> "Volvo"
 volvo_lightning.model
-#=> "Lightning"
+#=> "Lightning
+
+
+binding.pry
